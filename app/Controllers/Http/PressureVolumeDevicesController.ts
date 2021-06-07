a44061c7d8b630cc2cf7d7c7630bdbb894cd6a50
sigitnurmajid <sigitnurmajid32@gmail.com>
2021-06-07 18:07:22 +0700

    modify controller for enum type

--- app/Controllers/Http/PressureVolumeDevicesController.ts
+++ app/Controllers/Http/PressureVolumeDevicesController.ts
@@ -12,22 +12,18 @@ export default class PressureVolumeDevicesController {
     data.device_code = request.input('device_code')
     data.pressure_value = request.input('data.pressure_value')
     data.volume_value = request.input('data.volume_value')
-    data.time_device =  request.input('time_device')
+    data.time_device = request.input('time_device')
     data.status = request.input('data.status')
 
-    if ((request.input('data.status') == 'NORMAL') || (request.input('data.status') == 'LOWER') || (request.input('data.status') == 'UPPER')) {
-      
-      const device = await Device.findBy('device_code', data.device_code)
+    const device = await Device.findBy('device_code', data.device_code)
 
-      if(device){
-        await device.related('pressure_volume_device').save(data)
-        return response.status(200).send('Data Saved')
-      } else {
-        return response.status(400).send('Node not registered')
-      }
+    if (device) {
+      await device.related('pressure_volume_device').save(data)
+      return response.status(200).send('Data Saved')
     } else {
-      return response.status(400).send('Status not valid')
+      return response.status(400).send('Node not registered')
     }
+
   }
 
   public async store({ }: HttpContextContract) {
