08cf484284bd8693066174bfa3dedc9ab4c56765
sigitnurmajid <sigitnurmajid32@gmail.com>
2021-12-07 00:20:09 +0700

    add: show data for a first dashboard

--- app/Controllers/Http/PressureVolumeDevicesController.ts
+++ app/Controllers/Http/PressureVolumeDevicesController.ts
@@ -1,10 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import DataCollectionDevice from 'App/Models/DataCollectionDevice'
 import Device from 'App/Models/Device'
 import PressureVolumeDevice from 'App/Models/PressureVolumeDevice'
 import StatusMasterDatum from 'App/Models/StatusMasterDatum'
 import ThresholdDevice from 'App/Models/ThresholdDevice'
+import users from 'App/Models/users'
 import Node from 'App/Services/Node'
+import { query } from './RawQuery/PressureVolumeRaw'
 import VolumeRateDevicesController from './VolumeRateDevicesController'
 import VolumeUsagesController from './VolumeUsagesController'
 
@@ -51,8 +54,8 @@ export default class PressureVolumeDevicesController {
         if (data.status !== '-') node.sendAlarmPressureTelegram(data, 'SUPPLIER')
         const threshold = await ThresholdDevice.findBy('device_code', data.device_code)
 
-        if(threshold){
-          if(data.status === '-' && (data.pressure_value < threshold.hospital_low_threshold || data.pressure_value > threshold.hospital_high_threshold)){
+        if (threshold) {
+          if (data.status === '-' && (data.pressure_value < threshold.hospital_low_threshold || data.pressure_value > threshold.hospital_high_threshold)) {
             node.sendAlarmPressureTelegram(data, 'CLIENT')
           }
         }
@@ -81,12 +84,43 @@ export default class PressureVolumeDevicesController {
   public async store({ }: HttpContextContract) {
   }
 
-  public async show({ params, response }: HttpContextContract) {
-    const device = await PressureVolumeDevice.query().limit(params.id).orderBy('created_at', 'desc')
-      .catch(() => {
-        return response.status(400).send({ error: true, message: 'Error while querying' })
-      })
-    return device
+  public async show({ auth, response, request }: HttpContextContract) {
+
+    try {
+      const user = await users.findOrFail(auth.user?.id)
+      await user.load('organization')
+      await user.load('userRole')
+      await user.load('sites')
+
+      async function data() {
+        if (user.userRole.role === 'superadmin') {
+          const organization_id = request.qs().organization_id
+          if(!organization_id) throw new Error('Insert organization_id for superadmin role')
+
+          const deviceByOrganization = await Device.query().where('organization_id', organization_id)
+          const devicesEntity = deviceByOrganization.map(x => `\'${x.device_code}\'`).join(',')
+          return await Database.rawQuery(query(devicesEntity))
+
+        } else if (user.userRole.role === 'admin') {
+          const organization = user.organization
+          const deviceByOrganization = await Device.query().where('organization_id', organization.id)
+          const devicesEntity = deviceByOrganization.map(x => `\'${x.device_code}\'`).join(',')
+          return await Database.rawQuery(query(devicesEntity))
+
+        } else {
+          const site = user.sites
+          const deviceBySite = await Device.query().where('site_id', site[0].id)
+          const devicesEntity = deviceBySite.map(x => `\'${x.device_code}\'`).join(',')
+          return await Database.rawQuery(query(devicesEntity))
+        }
+      }
+      const resultData = await data()
+
+      return response.status(200).json({ code: 200, status: 'success' , data : resultData.rows})
+    } catch (error) {
+      return response.status(500).json({ code: 500, status: 'error', message: error.message })
+    }
+
   }
 
   public async edit({ }: HttpContextContract) {
---
+++ app/Controllers/Http/RawQuery/PressureVolumeRaw.ts
@@ -0,0 +1,19 @@
+const query = (params : string) => {
+  return `SELECT
+  n1.*,
+  n2.pressure_value,
+  n2.volume_value,
+  n2.stability_value,
+  devices.device_name,
+  devices.location,
+  sites.name
+  FROM
+  (SELECT DISTINCT ON (device_code) * FROM data_collection_devices where device_code IN (${params}) ORDER BY device_code, time_device DESC) n1
+  INNER JOIN (SELECT DISTINCT on (device_code) * FROM pressure_volume_devices where device_code IN (${params}) ORDER BY  device_code, time_device DESC) n2
+  ON n2.device_code = n1.device_code
+  INNER JOIN devices ON n2.device_code = devices.device_code
+  INNER JOIN sites ON sites.id = devices.site_id
+  `
+}
+
+export {query}
