6c4578e8b4c6b485ba4356ec6e2647bfff33d9ab
sigit nurmajid <sigitnurmajid32@gmail.com>
2022-02-27 19:25:47 +0700

    refactor: serialize filling model

--- app/Models/Filling.ts
+++ app/Models/Filling.ts
@@ -33,9 +33,9 @@ export default class Filling extends BaseModel {
   })
   public device: BelongsTo<typeof Device>
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true , serializeAs: null})
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
 }
